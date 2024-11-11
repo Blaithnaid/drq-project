d739d78508bcbcd412455629d538b8bd0911eb10
Blaithnaid <iarlaken@gmail.com>
2024-11-11 19:17:39 +0000

    imports for server

---
+++ src/backend/server.js
@@ -0,0 +1,7 @@
+// imports
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
