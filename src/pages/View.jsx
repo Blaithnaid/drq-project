0f72733e6ea8aca3a9eea67a90570ef6436d77c1
Blaithnaid <iarlaken@gmail.com>
2024-11-11 22:22:38 +0000

    import css, add header

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { Form, Button, Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./View.css";
 
-const Add = () => {
-	return <></>;
+const View = () => {
+	return (
+		<Container>
+			<br />
+			<h1>View</h1>
+		</Container>
+	);
 };
 
 export default View;
