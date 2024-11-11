8abc3a1a2c208c7dd3ed90c155034a6946c0668d
Blaithnaid <iarlaken@gmail.com>
2024-11-11 21:28:26 +0000

    just adding this for now

---
+++ src/pages/View.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Form, Button, Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Add = () => {
+	return <></>;
+};
+
+export default View;
