ce935661775a3b98062b3897e69a5bf5c1bcd3a7
Blaithnaid <iarlaken@gmail.com>
2024-11-11 19:22:57 +0000

    adding more setup for the server

--- src/backend/server.js
+++ src/backend/server.js
@@ -5,3 +5,30 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+
+// get environment variables
+require("dotenv").config();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+// we need to allow any origin to access our server, so we use CORS middleware
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, DELETE, OPTIONS"
+	);
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
+
+// connect to MongoDB
+mongoose.connect(process.env.DB_CONNECTION_STRING);
+// check if the connection is successful
+mongoose.connection.on("connected", () => {
+	console.log("Connected to MongoDB");
+});
