b12d85de945db6c73c198694b465fa1940496219
Blaithnaid <iarlaken@gmail.com>
2024-11-11 22:12:20 +0000

    linebreaks to space the form controls -_-

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,27 +1,29 @@
 import React from "react";
 import { Form, Button, Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Add.css";
 
 const Add = () => {
 	return (
 		<Container>
+			<br />
 			<h1>Add a Book</h1>
 			<Form>
 				<Form.Group controlId="formBookTitle">
 					<Form.Label>Title</Form.Label>
 					<Form.Control type="text" placeholder="Enter book title" />
 				</Form.Group>
-
+				<br />
 				<Form.Group controlId="formBookAuthor">
 					<Form.Label>Author</Form.Label>
 					<Form.Control type="text" placeholder="Enter book author" />
 				</Form.Group>
-
+				<br />
 				<Form.Group controlId="formBookGenre">
 					<Form.Label>Genre</Form.Label>
 					<Form.Control type="text" placeholder="Enter book genre" />
 				</Form.Group>
-
+				<br />
 				<Form.Group controlId="formBookDescription">
 					<Form.Label>Description</Form.Label>
 					<Form.Control
@@ -30,7 +32,7 @@ const Add = () => {
 						placeholder="Enter book description"
 					/>
 				</Form.Group>
-
+				<br />
 				<Button variant="primary" type="submit">
 					Submit
 				</Button>
