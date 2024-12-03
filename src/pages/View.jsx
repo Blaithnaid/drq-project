1d1d3a07ef8c9f6a7da629bd34af7036ea430c2d
Blaithnaid <iarlaken@gmail.com>
2024-12-03 21:29:57 +0000

    added search links for various book sites for each object

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -33,61 +33,115 @@ const View = () => {
 
 	return (
 		<Container>
-			<Form>
-				<Row>
-					{books.map((book) => (
-						<Col
-							key={book.id}
-							xs={12}
-							sm={6}
-							md={4}
-							style={{ marginBottom: "20px" }}
-						>
-							<Card>
-								<div className="d-flex justify-content-between align-items-center">
-									<Card.Img variant="top" src={book.cover} />
+			<Row>
+				{books.map((book) => (
+					<Col
+						key={book._id}
+						xs={12}
+						sm={6}
+						md={4}
+						style={{ marginBottom: "20px" }}
+					>
+						<Card>
+							<div className="d-flex justify-content-between align-items-center">
+								<Card.Img variant="top" src={book.cover} />
+							</div>
+							<Card.Body>
+								<Card.Title>{book.title}</Card.Title>
+								<Card.Subtitle>{book.author}</Card.Subtitle>
+								<Card.Text as="div">
+									<div>ID: {book._id}</div>
+									<div>Year: {book.year}</div>
+									<div>ISBN: {book.isbn}</div>
 									<div>
-										<Dropdown>
-											<Dropdown.Toggle
-												variant="success"
-												id="dropdown-basic"
-											>
-												Options
-											</Dropdown.Toggle>
-											<Dropdown.Menu>
-												<Dropdown.Item href="#">
-													Edit
-												</Dropdown.Item>
-												<Dropdown.Item href="#">
-													Delete
-												</Dropdown.Item>
-											</Dropdown.Menu>
-										</Dropdown>
+										Owned: {book.owned ? "Yes" : "No"}
 									</div>
+								</Card.Text>
+								<div className="d-flex justify-content-between">
+									<Dropdown className="flex-grow-1">
+										<Dropdown.Toggle
+											variant="primary"
+											id="dropdown-basic"
+										>
+											Options
+										</Dropdown.Toggle>
+										<Dropdown.Menu>
+											<Dropdown.Item
+												href="#"
+												onClick={() => {
+													console.log(
+														"Attempting to delete book with id:",
+														book._id
+													); // Add this line
+													if (
+														window.confirm(
+															"Are you sure you want to delete this book?"
+														)
+													) {
+														axios
+															.delete(
+																`http://localhost:4000/api/books/${book._id}`
+															)
+															.then(() => {
+																setBooks(
+																	books.filter(
+																		(b) =>
+																			b.id !==
+																			book._id
+																	)
+																);
+															})
+															.catch((error) => {
+																console.error(
+																	"Error deleting book:",
+																	error
+																);
+																alert(
+																	"Failed to delete the book. Please try again."
+																);
+															});
+													}
+												}}
+											>
+												Delete
+											</Dropdown.Item>
+										</Dropdown.Menu>
+									</Dropdown>
+									<Dropdown className="flex-grow-1">
+										<Dropdown.Toggle
+											variant="secondary"
+											id="dropdown-basic"
+										>
+											Search
+										</Dropdown.Toggle>
+										<Dropdown.Menu>
+											<Dropdown.Item
+												href={`https://www.amazon.com/s?k=${book.title}`}
+											>
+												Amazon
+											</Dropdown.Item>
+										</Dropdown.Menu>
+										<Dropdown.Menu>
+											<Dropdown.Item
+												href={`https://www.goodreads.com/search?q=${book.title}`}
+											>
+												Goodreads
+											</Dropdown.Item>
+										</Dropdown.Menu>
+										<Dropdown.Menu>
+											<Dropdown.Item
+												href={`https://www.google.com/search?udm=36&q=${book.title}`}
+											>
+												Google Books
+											</Dropdown.Item>
+										</Dropdown.Menu>
+									</Dropdown>
 								</div>
-								<Card.Body>
-									<Card.Title>{book.title}</Card.Title>
-									<Card.Text as="div">
-										<div>Author: {book.author}</div>
-										<div>Year: {book.year}</div>
-										<div>ISBN: {book.isbn}</div>
-										<div>
-											Owned: {book.owned ? "Yes" : "No"}
-										</div>
-									</Card.Text>
-									<Button
-										variant="primary"
-										href={`https://www.amazon.com/s?k=${book.title}`}
-									>
-										Search Amazon
-									</Button>
-									<Button variant="secondary">Edit</Button>
-								</Card.Body>
-							</Card>
-						</Col>
-					))}
-				</Row>
-			</Form>
+							</Card.Body>
+						</Card>
+					</Col>
+				))}
+			</Row>
 		</Container>
 	);
 };
