e73e0fbab684d6fb63b0def2930cc1e0908320f4
Blaithnaid <iarlaken@gmail.com>
2024-12-04 22:42:10 +0000

    styling

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -8,7 +8,7 @@ const BookCard = ({
 	handleDeleteClick,
 	formatTitleForUrl,
 }) => (
-	<Card>
+	<Card className="d-flex align-items-center">
 		<div className="d-flex justify-content-between align-items-center">
 			<Card.Img
 				variant="top"
